d9ccddad6203e798628577ee1065ac23466f141d
當麻 竜太 <ninzaburou.imaizumi@gmail.com>
2026-04-15 06:43:21 +0900

    all done

---
+++ pages/posts/firstPost.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Head from "next/head";
+
+export default function FirstPost() {
+  return (
+    <>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Link href="/">ホームへ戻る</Link>
+    </>
+  );
+}
